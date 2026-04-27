e4f8d7a80b751becddfe4ff4d146b9f26b59a2e7
gafs-codex <abdulmuizgafarcodes@gmail.com>
2026-04-27 10:02:52 +0100

    tapascript react course

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const header = <h1>JSX is awesome</h1>
+
+  return (
+    { header }
+  )
+}
+
+export default App
