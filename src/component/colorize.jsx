db63c4a13d8b11ab14dabe5753ec5809dcdb93e5
gafs-codex <abdulmuizgafarcodes@gmail.com>
2026-04-28 15:05:33 +0100

    playing with states

--- src/component/colorize.jsx
+++ src/component/colorize.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
+
+const profile = {
+    name: "gaf",
+    age: 25,
+    isAdmin: false
+}
+const list = ["code", "play game", "stretch"]
 function Colorize() {
     const [color, setColor] = useState('#ef4565')
     const [isOn, setIsOn] = useState(false)
     const [count, setCount] = useState(0)
+    const [name, setName] = useState("")
+    const [user, setUser] = useState(profile)
     const styles = {
         backgroundColor: color
     }
-
 
     const toggleSwitch = () => {
         setIsOn(prev => !prev)
@@ -22,6 +30,27 @@ function Colorize() {
     const increment = () => {
         setCount(prev => prev + 1)
     }
+
+    const onType = (event) => {
+        setName(event.target.value)
+    }
+
+    const birthday = () => {
+        setUser((prev) => {
+            return {
+                ...prev,
+                age: prev.age + 1
+            }
+        })
+    }
+    const makeAdmin = () => {
+        setUser((prev) => {
+            return {
+                ...prev,
+                isAdmin: true
+            }
+        })
+    }
     return (
         <>
             <h4>count is {count}</h4>
@@ -34,6 +63,27 @@ function Colorize() {
             <button className="mode-btn" onClick={toggleSwitch}>
                 {isOn ? "Light mode" : "Dark mode"}
             </button>
+
+            <div>
+                <input
+                    type="text"
+                    value={name}
+                    placeholder="Enter your name"
+                    onChange={onType}
+                />
+
+                {name && <h1>Hello, {name}</h1>}
+            </div>
+
+
+            <div className="object">
+                <h1>{user.name}</h1>
+                <p>Age: {user.age}</p>
+                <p>Admin: {user.isAdmin ? "Yes" : "No"}</p>
+                <button className="bday" onClick={birthday}>Happy Birthday 🎂</button>
+                <br />
+                <button className="admin" onClick={makeAdmin}>Make Admin</button>
+            </div>
         </>
     )
 }
